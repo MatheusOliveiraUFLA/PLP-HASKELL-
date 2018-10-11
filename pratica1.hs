c314ff9a2072b5f7ea57ae270d44bfdee66ec8b2
Aluno <matheus_olliverr@hotmail.com>
2018-10-11 10:45:07 -0300

    Potencia

--- pratica1.hs
+++ pratica1.hs
@@ -1,8 +1,13 @@
 soma :: Integer -> Integer -> Integer
 soma a b = a + b
 
+potencia :: Int -> Int -> Int
+potencia base 1 = base
+potencia base pot = base * (potencia base (pot -1))
 main = do
   print "Soma dois numeros"
   print $ soma 5 10
   print $ 5 `soma` 10
- -- sei la mano
+  print "Potencia pot(5,3)"
+  print $ potencia 5 3
+ 
